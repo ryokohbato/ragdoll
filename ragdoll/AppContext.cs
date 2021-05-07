8ea76e2dfb9fa78e5a6f389ee175c5df57204332
ryokohbato <ryokohbato@gmail.com>
2021-05-08 00:36:59 +0900

    feat: Setup

---
+++ ragdoll/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ragdoll
+{
+  public class AppContext : BindableBase
+  {
+    public MainModel MainModel { get; private set; }
+    public AppContext()
+    {
+      this.MainModel = new MainModel(this);
+    }
+  }
+}
---
+++ ragdoll/MainModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ragdoll
+{
+  public class MainModel : BindableBase
+  {
+    private AppContext appContext;
+    public MainModel(AppContext appContext)
+    {
+      this.appContext = appContext;
+    }
+  }
+}
